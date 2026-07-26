7aba74cd6ae3571b6726f441ffba03e802b36896
Carlos Alexandre Gomes <106932072+hilaliskandar@users.noreply.github.com>
2026-07-26 04:12:20 -0300

    fix: ativar correção de handles do FieldFile

--- applications/apps.py
+++ applications/apps.py
@@ -5,3 +5,8 @@ class ApplicationsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "applications"
     verbose_name = "Aplicações do Protocolo HIS"
+
+    def ready(self) -> None:
+        from .compatibilidade_arquivos import aplicar_correcao_fieldfile
+
+        aplicar_correcao_fieldfile()
